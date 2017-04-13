020a729c3eb3eb9862337667ce74c2ddb3efa7cf
mcharle <meryl_charleston@brown.edu>
2017-04-12 22:59:16 -0400

    cleaned up code and commented

--- vis/blogpost3.js
+++ vis/blogpost3.js
@@ -1,6 +1,6 @@
 /////////////////////////////////////
-// Step 1: Write accessor functions //
-//////////////////////////////////////
+// Some code copied from d3 project//
+/////////////////////////////////////
 
 
 
@@ -11,7 +11,6 @@ var height = 500 - margin.top - margin.bottom;
 
 
 // Various scales
-
 var xScale = d3.scaleLinear().domain([-73514, -69988]).range([0, width]),
     yScale = d3.scaleLinear().domain([41244, 42871]).range([height, 0]),
     colorScale = d3.scaleOrdinal([0,1]);
@@ -20,49 +19,16 @@ var xScale = d3.scaleLinear().domain([-73514, -69988]).range([0, width]),
 var xAxis = d3.axisBottom(xScale),
     yAxis = d3.axisLeft(yScale);
 
-// Create the SVG container and set the origi
-
-//////////////////////////////
-// Step 2: Add x and y axes //
-//////////////////////////////
-  // svg.append("g")
-  //   .attr("class", "axis line")
-  //   .call(yAxis)
-  //   .append("g")
-  //   .attr("class", "axis line")
-  //   .attr("transform", "translate(0, " + height + ")")
-  //   .call(xAxis);
-
-//////////////////////////////////////
-// Step 3: Add axis and year labels //
-//////////////////////////////////////
-
-// var x = svg.append("text")
-//     .text("x-axis")
-//     .attr("class", "label")
-//     .attr("transform", "translate(" + height + "," + (height + 15) + ")");
-
-// var y = svg.append("text")
-//     .text("y-axis")
-//     .attr("class", "label")
-//     .attr("transform", "translate(" + -1*margin.left + "," + height/2 + ")")
-
-///////////////////////////
-// Step 4: Load the data //
-///////////////////////////
 
 // Load the data.
 d3.json("../data/basic_chars_cleaned.json", function(data) {
 
   data = data.filter(function(d) {
+    // we're only using one year for now
     return d.year == 2013;
   })
 
   console.log(d3.schemeCategory10[colorScale(data[0].charter)])
-
-  /////////////////////////////////////////
-  // Functions provided for your utility //
-  /////////////////////////////////////////
 
   var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -83,9 +49,6 @@ d3.json("../data/basic_chars_cleaned.json", function(data) {
         return d3.schemeCategory10[colorScale(d.charter)];
       })
 
-  ///////////////////////////////////
-  // Step 5: Add fluff and overlay //
-  ///////////////////////////////////
 
   // Add a title.
 
@@ -97,23 +60,23 @@ d3.json("../data/basic_chars_cleaned.json", function(data) {
 
 
   function x(d) {
-    // Return nation's income
+    // Return school's longitude
     return d.long*1000;
   }
   function y(d) {
-      // Return nation's lifeExpectancy
+      // Return school's latitude
       return d.lat*1000;
   }
   function radius(d) {
-      // Return nation's population
+      // nothing for now
       return 5;
   }
   function color(d) {
-      // Return nation's region
+      // Return school's charter/noncharter status
       return d.charter;
   }
   function key(d) {
-      // Return nation's name
+      // Return school's name
       return d.name;
   }
 
