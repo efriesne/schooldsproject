151181d801fcd8aa60e1e5723b227de238d60535
mcharle <meryl_charleston@brown.edu>
2017-04-25 17:11:49 -0400

    join two csv files and change color coding from charter to success

--- vis/blogpost4.js
+++ vis/blogpost4.js
@@ -20,98 +20,132 @@ var xAxis = d3.axisBottom(xScale),
 
 
 // Load the data.
-d3.csv("data/basic_chars_cleaned.csv", function(data) {
+d3.csv("../data/final_data/basic_chars_cleaned_ids.csv", function(locations) {
 
-  data = data.filter(function(d) {
-    // we're only using one year for now
-    return d.year == 2013;
-  })
+  d3.csv("../data/final_data/success_metric.csv", function(successes) {
 
-
-  var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      
-  var dot = svg.append("g")
-      .attr("class", "dots")
-    .selectAll("class", "dot")
-      .data(data)
-    .enter()
-      .append("circle")
-      .attr("class", "dot")
-      .call(position)
-      .attr("fill", function(d) {
-        return d3.schemeCategory10[colorScale(d.charter)];
-      })
-
-  var zoom = d3.zoom()
-    .scaleExtent([1, 40])
-    .translateExtent([[-100, -100], [width + 90, height + 100]])
-    .on("zoom", zoomed);
-
-  var view = svg.append("rect")
-    .attr("class", "view")
-    .attr("x", 0.5)
-    .attr("y", 0.5)
-    .attr("width", width - 1)
-    .attr("height", height - 1)
-    .attr("visibility", "hidden");
-
-  var gX = svg.append("g")
-    .attr("class", "axis axis--x")
-    .call(xAxis)
-    .attr("visibility", "hidden");
-
-  var gY = svg.append("g")
-    .attr("class", "axis axis--y")
-    .call(yAxis)
-    .attr("visibility", "hidden");
-
-  // Add a title.
-
-
-  dot.append("title")
-    .text(function(d) {
-      return d.name;
+    var data = innerJoin(locations, successes, function(location, success) {
+      if (location.year === success.year && location.school_id === success.school_id) {  
+        return {
+            id: location.school_id,
+            name: location.school,
+            year: location.year,
+            charter: location.charter,
+            level: location.level,
+            town: location.town,
+            lat: location.lat,
+            long: location.long,
+            success: success.success,
+            ela_success: success.ela_success,
+            math_success: success.math_success
+            };
+      };
     })
 
-  function zoomed() {
-    svg.attr("transform", d3.event.transform);
-    xAxis.scale(d3.event.transform.rescaleX(xScale));
-    yAxis.scale(d3.event.transform.rescaleY(yScale));
-  }
-
-  function x(d) {
-    // Return school's longitude
-    return d.long*1000;
-  }
-  function y(d) {
-      // Return school's latitude
-      return d.lat*1000;
-  }
-  function radius(d) {
-      // nothing for now
-      return 5;
-  }
-  function color(d) {
-      // Return school's charter/noncharter status
-      return d.charter;
-  }
-  function key(d) {
-      // Return school's name
-      return d.name;
-  }
-
-  svg.call(zoom);
+    data = data.filter(function(d) {
+      // we're only using one year for now
+      return d.year == 2013;
+    })
 
 
-  function position(dot) {
-    dot .attr("cx", function(d) {
-      return xScale(x(d)); })
-        .attr("cy", function(d) { return yScale(y(d)); })
-        .attr("r", function(d) { return radius(d); });
-}
+    var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
+    var dot = svg.append("g")
+        .attr("class", "dots")
+      .selectAll("class", "dot")
+        .data(data)
+      .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .call(position)
+        .attr("fill", function(d) {
+          return d3.schemeCategory10[colorScale(color(d))];
+        })
 
+    var zoom = d3.zoom()
+      .scaleExtent([1, 40])
+      .translateExtent([[-100, -100], [width + 90, height + 100]])
+      .on("zoom", zoomed);
+
+    var view = svg.append("rect")
+      .attr("class", "view")
+      .attr("x", 0.5)
+      .attr("y", 0.5)
+      .attr("width", width - 1)
+      .attr("height", height - 1)
+      .attr("visibility", "hidden");
+
+    var gX = svg.append("g")
+      .attr("class", "axis axis--x")
+      .call(xAxis)
+      .attr("visibility", "hidden");
+
+    var gY = svg.append("g")
+      .attr("class", "axis axis--y")
+      .call(yAxis)
+      .attr("visibility", "hidden");
+
+    // Add a title.
+
+
+    dot.append("title")
+      .text(function(d) {
+        return d.name;
+      })
+
+    function zoomed() {
+      svg.attr("transform", d3.event.transform);
+      xAxis.scale(d3.event.transform.rescaleX(xScale));
+      yAxis.scale(d3.event.transform.rescaleY(yScale));
+    }
+
+    function x(d) {
+      // Return school's longitude
+      return d.long*1000;
+    }
+    function y(d) {
+        // Return school's latitude
+        return d.lat*1000;
+    }
+    function radius(d) {
+        // nothing for now
+        return 5;
+    }
+    function color(d) {
+        // Return school's charter/noncharter status
+        return d.success;
+    }
+    function key(d) {
+        // Return school's name
+        return d.name + " " + d.success;
+    }
+
+    svg.call(zoom);
+
+    function position(dot) {
+      dot .attr("cx", function(d) {
+        return xScale(x(d)); })
+          .attr("cy", function(d) { return yScale(y(d)); })
+          .attr("r", function(d) { return radius(d); });
+    }
+
+    function innerJoin(a, b, select) {
+      var m = a.length, n = b.length, c = [];
+
+      for (var i = 0; i < m; i++) {
+          var x = a[i];
+
+          for (var j = 0; j < n; j++) { // cartesian product - all combinations
+              var y = select(x, b[j]);  // filter out the rows and columns you want
+              if (y) c.push(y);         // if a row is returned add it to the table
+          }
+      }
+
+      return c;
+    }
+  })
 });
